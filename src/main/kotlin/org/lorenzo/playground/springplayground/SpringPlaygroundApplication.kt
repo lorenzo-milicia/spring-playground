5dd2821020e393b510581eade4f444c071b1b832
Lorenzo Milicia <lorenzo.milicia.it@gmail.com>
2022-06-12 21:44:51 +0200

    Get bean from context and use it

--- src/main/kotlin/org/lorenzo/playground/springplayground/SpringPlaygroundApplication.kt
+++ src/main/kotlin/org/lorenzo/playground/springplayground/SpringPlaygroundApplication.kt
@@ -1,9 +1,14 @@
 package org.lorenzo.playground.springplayground
 
 import org.lorenzo.playground.config.SpringPlaygroundConfig
+import org.lorenzo.playground.domain.services.ISpringPlaygroundService
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 fun main() {
 	val context: ApplicationContext = AnnotationConfigApplicationContext(SpringPlaygroundConfig::class.java)
+
+	val service: ISpringPlaygroundService = context.getBean(ISpringPlaygroundService::class.java)
+
+	service.doSomething()
 }
